288440def4c592d54f20612ec3f662be6f68100c
KRochas <karinerocha.s@hotmail.com>
2018-02-16 17:19:11 -0200

    Consumindo lista de filmes de API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpModule } from "@angular/http"
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { FeedPage } from './../pages/feed/feed';
+import { FeedPageModule } from '../pages/feed/feed.module';
+import { IntroPageModule } from '../pages/intro/intro.module';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { FeedPageModule } from '../pages/feed/feed.module';
-import { IntroPageModule } from '../pages/intro/intro.module';
+import { MovieProvider } from '../providers/movie/movie';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,8 @@ import { IntroPageModule } from '../pages/intro/intro.module';
     BrowserModule,
     IonicModule.forRoot(MyApp),
     FeedPageModule,
-    IntroPageModule
+    IntroPageModule,
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -40,7 +43,8 @@ import { IntroPageModule } from '../pages/intro/intro.module';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MovieProvider
   ]
 })
 export class AppModule {}
--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MovieProvider } from '../../providers/movie/movie';
 
 /**
  * Generated class for the FeedPage page.
@@ -12,13 +13,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+    MovieProvider
+  ]
 })
 export class FeedPage {
+
+  objeto_feed = {
+    titulo: "Karine Rocha",
+    data: "November 5, 1955",
+    descricao: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine",
+    qtd_likes: 12,
+    qtd_comments: 4,
+    time_coment: "11h ago"
+  }
+
+  public listaFilmes = new Array<any>();
 
   nomeUsuario: string = "Karine Rocha Código";
 
   
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+		public navCtrl: NavController,
+		public navParams: NavParams,
+		private movieProvider: MovieProvider
+    ) {
   }
   soma(n1: number, n2: number) {
     alert( n1+n2);
@@ -26,7 +45,16 @@ export class FeedPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FeedPage');
-   // this.soma(1,2);
-  }
-
+	// this.soma(1,2);
+	this.movieProvider.getLatestMovies().subscribe(
+		data => {
+			const response = (data as any); // transformou a resposta em um objeto de qualquer tipo para pegar qualquer valor de dentro
+			const objeto_retorno = JSON.parse(response._body); // transformou em JSON.parse  para transformar o que seria um texto em JSON.
+			this.listaFilmes = objeto_retorno.results;
+			console.log(objeto_retorno);
+		}, error => {
+			console.log(error);
+		})
+	}
 }
+
