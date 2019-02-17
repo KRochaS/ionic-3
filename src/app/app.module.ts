6e8dc4e053b9945b4b7d520cf8082fb922381e18
KRochaS <karinerocha.s@hotmail.com>
2019-02-17 15:20:16 -0300

    fix dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,28 @@
-import { PerfilPageModule } from './../pages/perfil/perfil.module';
-import { SobrePageModule } from './../pages/sobre/sobre.module';
-import { ConfiguracoesPageModule } from './../pages/configuracoes/configuracoes.module';
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HttpModule } from "@angular/http"
+import { PerfilPageModule } from "./../pages/perfil/perfil.module";
+import { SobrePageModule } from "./../pages/sobre/sobre.module";
+import { ConfiguracoesPageModule } from "./../pages/configuracoes/configuracoes.module";
+import { NgModule, ErrorHandler } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+import { MyApp } from "./app.component";
+import { HttpModule } from "@angular/http";
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-import { FeedPage } from './../pages/feed/feed';
-import { FeedPageModule } from '../pages/feed/feed.module';
-import { IntroPageModule } from '../pages/intro/intro.module';
+import { AboutPage } from "../pages/about/about";
+import { ContactPage } from "../pages/contact/contact";
+import { HomePage } from "../pages/home/home";
+import { TabsPage } from "../pages/tabs/tabs";
+import { FeedPage } from "./../pages/feed/feed";
+import { FeedPageModule } from "../pages/feed/feed.module";
+import { IntroPageModule } from "../pages/intro/intro.module";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { MovieProvider } from '../providers/movie/movie';
-import { LoginPageModule } from '../pages/login/login.module';
-import { FilmeDetalhesPageModule } from '../pages/filme-detalhes/filme-detalhes.module';
+import { StatusBarOriginal } from "@ionic-native/status-bar";
+import { SplashScreenOriginal } from "@ionic-native/splash-screen";
+import { MovieProvider } from "../providers/movie/movie";
+import { LoginPageModule } from "../pages/login/login.module";
+import { FilmeDetalhesPageModule } from "../pages/filme-detalhes/filme-detalhes.module";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-  ],
+  declarations: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
@@ -51,9 +45,9 @@ import { FilmeDetalhesPageModule } from '../pages/filme-detalhes/filme-detalhes.
     FeedPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    StatusBarOriginal,
+    SplashScreenOriginal,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
     MovieProvider
   ]
 })
